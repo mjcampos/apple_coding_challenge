6140936d2c7ba92ef5440e73f804a144a1cc360d
Michael Campos <camposmj89@gmail.com>
2019-06-30 23:53:58 -0700

    Created, and setup, the page task#new page to create new tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "tasks#index"
 
-  resources :tasks, only: %i[index]
+  resources :tasks, except: %i[show destroy]
   resources :kinds, except: %i[show destroy]
 end
