8fca02b4c5d882c89ce474bd71f7f3230ec42fd8
Michael Campos <camposmj89@gmail.com>
2019-06-30 23:58:57 -0700

    Build out tasks#create method in Task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,28 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    task = Task.create(task_params)
+    if task.valid?
+      if task.save
+        flash[:notice] = "Task good enough!"
+        redirect_to tasks_path
+      else
+        @task = task
+        render :new
+      end
+    else
+      flash[:notice] = "Task not filled in properly"
+      @task = task
+      render :new
+    end
+  end
+
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :category, :delivery_at, :description, :kind_id)
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,7 @@ class Task < ApplicationRecord
   belongs_to :kind
 
   validates :name, presence: true
+  validates :description, presence: true
 
   # missing category here with: development, backlog, review, administrative
 end
