fa317cabc82dbfba1d265e90caaf0794546c2fa7
Michael Campos <camposmj89@gmail.com>
2019-06-30 23:03:03 -0700

    Edited the Kind model to set a minimum size value based on the return value of the min_size method. Also added a name validation requirement stating that it must be a minimum length of 5

--- app/models/kind.rb
+++ app/models/kind.rb
@@ -1,9 +1,10 @@
 class Kind < ApplicationRecord
-  def min_size
+  def self.min_size
     20
   end
 
   def color; "blue"; end
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, length: { minimum: 5 }, presence: true, uniqueness: true
+  validates :size, numericality: { greater_than_or_equal_to: min_size }
 end
